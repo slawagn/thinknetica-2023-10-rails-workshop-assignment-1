d183f990b63568eb4fd42cb10edf4a8a8179b657
slawagn <slawagn@gmail.com>
2023-10-31 20:28:04 +0000

    change cloud payment fail logic to random

--- app/models/cloud_payment.rb
+++ app/models/cloud_payment.rb
@@ -1,6 +1,6 @@
 class CloudPayment < ApplicationRecord
   def self.process(user_uid:, amount_cents:, currency:)
-    return { status: :failed } if amount_cents == 0
+    return { status: :failed } if [0, 1].sample == 1
     
     cloud_payment = CloudPayment.create(user_uid: user_uid, amount_cents: amount_cents, currency: currency)
 

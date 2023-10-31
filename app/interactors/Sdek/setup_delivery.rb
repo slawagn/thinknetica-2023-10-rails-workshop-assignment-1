6d05586f096b2f9879143b8563de524c8b2aea60
slawagn <slawagn@gmail.com>
2023-10-31 20:30:19 +0000

    add random fail to Sdek::SetupDelivery

--- app/interactors/Sdek/setup_delivery.rb
+++ app/interactors/Sdek/setup_delivery.rb
@@ -8,5 +8,12 @@ class Sdek::SetupDelivery
       Person: #{context.person}
       Weight: #{context.weight}
     INFO
+
+    case [0, 1].sample
+    when 0
+      { result: :failed }
+    when 1
+      { result: :succeeded }
+    end
   end
 end
